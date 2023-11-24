a71ff4593d13162d7b0d699020e152f579e9a022
KokilaSidhalingam <abikoki27@gmail.com>
2023-11-24 13:18:42 +0530

    index.html,style.css and script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let list1 = [];
+let list2 = [];
+let list3 = [];
+
+let n = 0;
+let x = 0;
+function AddRow(){
+    let addrow = document.getElementById("show");
+    var newrow = addrow.insertRow(n);
+    
+// Collection of data from inputand stored in table 
+list1[x] = document.getElementById("fname").value;
+list2[x] = document.getElementById("lname").value;
+list3[x] = document.getElementById("email").value;
+
+// Inserting new row
+let cell1 = newrow.insertCell(0);
+let cell2 = newrow.insertCell(1);
+let cell3 = newrow.insertCell(2);
+
+cell1.innerHTML = list1[x];
+cell2.innerHTML = list2[x];
+cell3.innerHTML = list3[x];
+n++;
+x++
+}
